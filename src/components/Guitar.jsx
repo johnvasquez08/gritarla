c23fc6ff84166fbfcc0d29ebe8b93a201ac80718
johnvasquez08 <johndy0801939@gmail.com>
2024-10-24 18:26:34 -0500

    update

---
+++ src/components/Guitar.jsx
@@ -0,0 +1,20 @@
+export default function Guitar({guitar}) {
+    console.log(guitar)
+    const { name, description, price } = guitar
+    return (
+        <div className="col-md-6 col-lg-4 my-4 row align-items-center">
+                <div className="col-4">
+                    <img className="img-fluid" src="./public/img/guitarra_02.jpg" alt="imagen guitarra" />
+                </div>
+                <div className="col-8">
+                    <h3 className="text-black fs-4 fw-bold text-uppercase">{name}</h3>
+                    <p>{description}</p>
+                    <p className="fw-black text-primary fs-3">${price}</p>
+                    <button 
+                        type="button"
+                        className="btn btn-dark w-100"
+                    >Agregar al Carrito</button>
+                </div>
+            </div>
+    )
+}

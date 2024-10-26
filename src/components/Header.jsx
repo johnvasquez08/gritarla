d6bae44a58e8f0c4875f29ef3ac085c6d20b33a0
johnvasquez08 <johndy0801939@gmail.com>
2024-10-26 13:00:45 -0500

    actualizacion logos bug

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,14 +11,14 @@ const carTotal = useMemo(() => cart.reduce((total, item) => total + (item.quanti
             <div className="row justify-content-center justify-content-md-between">
                 <div className="col-8 col-md-3">
                     <a href="index.html">
-                        <img className="img-fluid" id='logo' src="./public/img/logo.png" alt="imagen logo" />
+                        <img className="img-fluid" id='logo' src="./img/logo.png" alt="imagen logo" />
                     </a>
                 </div>
                 <nav className="col-md-6 a mt-5 d-flex align-items-start justify-content-end">
                     <div 
                         className="carrito"
                     >
-                        <img className="img-fluid" src="./public/img/carrito.png" alt="imagen carrito" />
+                        <img className="img-fluid" src="./img/carrito.png" alt="imagen carrito" />
 
                         <div id="carrito" className="bg-white p-3">
                             {isEmpty ? (
